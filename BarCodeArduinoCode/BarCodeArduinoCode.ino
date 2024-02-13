5d43c3d6580a8bee02532307f89bf138dc061940
Andrey Lisov <42667249+AnLiMan@users.noreply.github.com>
2024-02-13 12:59:56 +0500

    Update BarCodeArduinoCode.ino

--- BarCodeArduinoCode/BarCodeArduinoCode.ino
+++ BarCodeArduinoCode/BarCodeArduinoCode.ino
@@ -9,7 +9,8 @@ void setup() {
   pinMode(Action1, OUTPUT);
   pinMode(Action2, OUTPUT);
   pinMode(Action3, OUTPUT);
-  pinMode(13, OUTPUT);
+
+  Serial.setTimeout(1);
 
   digitalWrite(Action1, LOW);
   digitalWrite(Action2, LOW);
@@ -17,27 +18,26 @@ void setup() {
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    data = Serial.read();
-
-    //Если приняли ноль
-    if (data == '0') {
-      digitalWrite(Action1, HIGH);
-      digitalWrite(Action2, LOW);
-      digitalWrite(Action3, LOW);
-    }
-    //Если приняли 1
-    else if (data == '1') {
-      digitalWrite(Action1, LOW);
-      digitalWrite(Action2, HIGH);
-      digitalWrite(Action3, LOW);
-    }
-    //Если приняли 2
-    else if (data == '2') {
-      digitalWrite(Action1, LOW);
-      digitalWrite(Action2, LOW);
-      digitalWrite(Action3, HIGH);
-      digitalWrite(13, HIGH);
-    }
+  while (!Serial.available());
+  data = Serial.readString().toInt();
+  Serial.print(data);
+  
+  //Если приняли ноль
+  if (data == 0) {
+    digitalWrite(Action1, HIGH);
+    digitalWrite(Action2, LOW);
+    digitalWrite(Action3, LOW);
+  }
+  //Если приняли 1
+  else if (data == 1) {
+    digitalWrite(Action1, LOW);
+    digitalWrite(Action2, HIGH);
+    digitalWrite(Action3, LOW);
+  }
+   //Если приняли 2
+  else if (data == 2) {
+    digitalWrite(Action1, LOW);
+    digitalWrite(Action2, LOW);
+    digitalWrite(Action3, HIGH);
   }
 }
